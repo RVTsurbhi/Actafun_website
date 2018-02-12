ca7ca34e1130f87b842758593a8e43b2422b2def
surbhi <surbhi.henceforth@gmail.com>
2018-02-12 08:02:02 +0530

    navbar-fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,51 @@
-// Empty JS for your own code to be here
+$(document).ready(function(){
+
+  $('.image-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+//navbar-fixed and color change on scroll
+  $(window).on("scroll", function() {
+      if($(window).scrollTop() > 300) {
+          $(".navbar-fixed-top").addClass("active");
+      } else {
+          //remove the background property so it comes transparent again (defined in your css)
+         $(".navbar-fixed-top").removeClass("active");
+      }
+  });
+
+  //bottobar section fixed on scroll
+    $(window).scroll(function(){
+      if ($(window).scrollTop() > 310) {
+          $(".bottom").addClass("fixed");
+      } else {
+          $(".bottom").removeClass("fixed");
+      }
+    });
+//section about fixed on scroll
+$(window).scroll(function(){
+  if ($(window).scrollTop() > 310) {
+      $(".stableBtn").addClass("stable");
+  } else {
+      $(".stableBtn").removeClass("stable");
+  }
+});
+  //img_bottom section fixed on scroll
+    $(window).scroll(function(){
+      if ($(window).scrollTop() > 300) {
+          $(".burner").addClass("fixed1");
+      } else {
+          $(".burner").removeClass("fixed1");
+      }
+    });
+
+  //class change on click
+      $(".heart_icon").on('click',function(){
+      $(this).css({"color": "red"}).removeClass('fa-heart-o').addClass('fa-heart');
+    });
+
+
+});
